1bf67b184b42d8e1e8ae6728063280e9c5676d4a
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2021-07-25 12:21:31 -0300

    Initial Commit

---
+++ stanford-CS193p/stanford_CS193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  stanford_CS193pApp.swift
+//  stanford-CS193p
+//
+//  Created by Guilherme Nascimento on 25/07/21.
+//
+
+import SwiftUI
+
+@main
+struct stanford_CS193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
