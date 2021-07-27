e0ffae26b45c0e8e7c29c418e718ac46afa4a26b
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2021-07-27 02:06:53 -0300

    Lecture 2: Creating reusable components (a Card in the game) and combining them to make more complex user-interfaces.

--- stanford-CS193p/ContentView.swift
+++ stanford-CS193p/ContentView.swift
@@ -8,21 +8,94 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = [
+        "🐶", "🦁", "🐸", "🦊", "🐹", "🐵",
+        "🐭", "🐔", "🐝", "🦄", "🦆", "🐷",
+        "🐮", "🐤", "🐗", "🐺", "🦉", "🐞",
+        "🦂", "🦐", "🐡", "🐬", "🐆", "🐊"
+    ]
+    @State var emojiCount: Int = 6
+    
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                .stroke(lineWidth: 3)
-            
-            Text("Meu app")
-                .foregroundColor(.yellow)
+        VStack {
+            HStack {
+                Text("Memorize")
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            }
+            ScrollView{
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .padding(.horizontal)
+            .font(.largeTitle)
         }
         .padding(.all)
         .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct CardView: View {
+    
+    var content: String
+    @State var isFaceUp: Bool = false
+    var body: some View {
+        
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+            
+            if(isFaceUp){
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }.onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+        
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
